059f457958619c279296242235c916968b6a5d01
OleksiyTymchenko <super.alextim@gmail.com>
2024-08-13 19:39:51 +0300

    Almost done first task

--- script.js
+++ script.js
@@ -1,23 +1,59 @@
 "use strict;"
 
-// document.write('Hello world');
-// document.title = ' Alex best!!!'
-// let h1 = document.getElementsByClassName('main-title');
-// let h2 = document.getElementById('title');
-// let p2 = document.getElementsByTagName('h1');
-// console.log(h2);
+// Масив з назвами пісень
+const songs = [
+    "Пісня 1: Назва пісні 1",
+    "Пісня 2: Назва пісні 2",
+    "Пісня 3: Назва пісні 3",
+    "Пісня 4: Назва пісні 4",
+    "Пісня 5: Назва пісні 5",
+  ];
 
-// const $title = document.getElementById('list');
-//  $title = 'Hello Alex';
+  let playList = [
+    {
+     author: "LED ZEPPELIN",
+     song:"STAIRWAY TO HEAVEN"
+    },
+    {
+     author: "QUEEN",
+     song:"BOHEMIAN RHAPSODY"
+    },
+    {
+     author: "LYNYRD SKYNYRD",
+     song:"FREE BIRD"
+    },
+    {
+     author: "DEEP PURPLE",
+     song:"SMOKE ON THE WATER"
+    },
+    {
+     author: "JIMI HENDRIX",
+     song:"ALL ALONG THE WATCHTOWER"
+    },
+    {
+     author: "AC/DC",
+     song:"BACK IN BLACK"
+    },
+    {
+     author: "QUEEN",
+     song:"WE WILL ROCK YOU"
+    },
+    {
+     author: "METALLICA",
+     song:"ENTER SANDMAN"
+    }
+    ];
 
+  // Отримуємо елемент списку
+  const songList = document.getElementById("songList");
 
-// const textParaph = document.getElementById('title');
-// textParaph.innerHTML = '<h1><span class="jack">Alex</span> you did it.</h1>'
-// console.log(copy);
+  // Додаємо кожну пісню до списку
+  playList.forEach((song) => {
+    const li = document.createElement("li"); // Створюємо новий елемент списку
+    li.textContent = `${song.author}    - ${song.song}`; // Встановлюємо текст елемента
+    // const authorName = li.textContent = song.author; // Встановлюємо текст елемента
+    // const authorSong = li.textContent = song.song; // Встановлюємо текст елемента
+    songList.appendChild(li); // Додаємо елемент до списку
+  });
 
-const menuButton = document.getElementById('name');
-const menuName = document.getElementById('title');
-
-menuButton.addEventListener('click', () => {
-    menuName.classList.toggle('close');
-})
+  
